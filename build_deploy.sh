d67b29b7f04a544cb57bdb4cf0052196ddc06865
Riccardo piccoli <rpiccoli@redhat.com>
2023-02-02 09:50:54 +0100

    add deploy script
    Signed-off-by: Riccardo piccoli <rpiccoli@redhat.com>

---
+++ build_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -exu
+
+TAG=$(git rev-parse --short=7 HEAD)
+export IMAGE_NAME="quay.io/app-sre/assisted-events-stream"
+export IMAGE_TAG=$TAG
+export DOCKER_BUILDKIT=1
+
+make docker-build
+
+DOCKER_CONF="$PWD/assisted/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
+docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${REPO}:latest"
+docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
+docker --config="$DOCKER_CONF" push "${REPO}:latest"
