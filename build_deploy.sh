bdbcc18a510bf9cc3de149cb5eed6e4da35bb8a0
Riccardo piccoli <rpiccoli@redhat.com>
2023-02-06 09:51:13 +0100

    clean template: remove null annotations and helm references
    Signed-off-by: Riccardo piccoli <rpiccoli@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -6,10 +6,6 @@ TAG=$(git rev-parse --short=7 HEAD)
 export IMAGE_NAME="quay.io/app-sre/assisted-events-stream"
 export IMAGE_TAG=$TAG
 export DOCKER_BUILDKIT=1
-export REDIS_EXPORTER_IMAGE_NAME="quay.io/app-sre/assisted-redis"
-export REDIS_IMAGE_NAME="quay.io/app-sre/assisted-redis-exporter"
-export REDIS_IMAGE_TAG="6.2.7-debian-10-r23"
-export REDIS_EXPORTER_IMAGE_TAG="1.37.0-debian-10-r63"
 
 make docker-build
 make redis-docker-build
@@ -20,6 +16,3 @@ docker --config="$DOCKER_CONF" login -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.i
 docker tag "${IMAGE_NAME}:${IMAGE_TAG}" "${IMAGE_NAME}:latest"
 docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:${IMAGE_TAG}"
 docker --config="$DOCKER_CONF" push "${IMAGE_NAME}:latest"
-
-#docker --config="$DOCKER_CONF" push "${REDIS_EXPORTER_IMAGE_NAME}:${REDIS_EXPORTER_IMAGE_TAG}"
-#docker --config="$DOCKER_CONF" push "${REDIS_IMAGE_NAME}:${REDIS_IMAGE_TAG}"
